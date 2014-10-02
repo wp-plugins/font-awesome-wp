eb9056b06cc47228ed0f3e489828a1c63fc468ac
farjana55 <farjana55@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-02 11:02:46 +0000

    upload
    git-svn-id: https://plugins.svn.wordpress.org/font-awesome-wp/trunk@1000529 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ font.php
@@ -0,0 +1,50 @@
+<?php
+/**
+Plugin Name: Font Awesome WP
+Plugin URI: http://freelancezonebd.com/font-awesome-plugin/
+Description: This plugin is used for  font awesome icons which is updated version with various styles that more user friendly.
+Author: Farjana Rashid
+Version: 1.0
+Author URI: http://farjana-rashid.com
+*/
+
+function font_awesome_files_included() {
+    wp_enqueue_style( 'font-awesome-css', plugins_url( '/css/font-awesome.min.css', __FILE__ ));
+}
+add_action('init','font_awesome_files_included');
+
+add_filter('widget_text', 'do_shortcode');
+
+function font_awesome_styles($atts){
+	extract ( shortcode_atts ( array (
+		'color' => '#555',
+		'border_color' => '#555',
+		'icon' => '',
+		'font' => '14px',
+		'width' => '12px',
+		'padding' => '5px',
+	), $atts, 'font') );
+
+	return '<i style="font-size: '.$font.'; color:'.$color.';border: 1px solid '.$border_color.';padding: '.$padding.';border-radius: 50%;margin: 2px;text-align:center;width:'.$width.';height:'.$width.'"  class="fa '.$icon.'"></i>';
+
+}
+add_shortcode('font','font_awesome_styles');
+
+function font_awesome_styles_with_link($atts){
+	extract ( shortcode_atts ( array (
+		'color' => '#555',
+		'border_color' => '#555',
+		'icon' => '',
+		'font' => '14px',
+		'href' => '#',
+		'hover_color' => '#fff',
+		'hover_bg' => '#555',
+		'width' => '12px',
+		'padding' => '5px',
+	), $atts, 'font_link') );
+
+	return '<a href="'.$href.'" class="font_awesome_link" style="color:'.$color.';margin: 2px;outline: medium none;text-decoration:none"><i style="text-align:center;width:'.$width.';height:'.$width.';border: 1px solid '.$border_color.';border-radius: 50%;font-size: '.$font.';padding: '.$padding.';"  class="fa '.$icon.'"></i></a><style>a.font_awesome_link:hover{color:'.$hover_color.' !important;}a.font_awesome_link:hover i{border:1px solid '.$hover_bg.' !important;background:'.$hover_bg.' }</style>';
+
+}
+add_shortcode('font_link','font_awesome_styles_with_link');
+?>
