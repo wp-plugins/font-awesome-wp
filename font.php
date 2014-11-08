65f34aff36bbf2bf78f5e96f7173e89b502030e2
farjana55 <farjana55@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-08 08:44:51 +0000

    update
    git-svn-id: https://plugins.svn.wordpress.org/font-awesome-wp/trunk@1021904 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- font.php
+++ font.php
@@ -6,8 +6,24 @@ Description: This plugin is used for font awesome updated version.You can use va
 Author: Farjana Rashid
 Version: 1.0
 Author URI: http://farjana-rashid.com
+License: GPLv2 or later
 */
 
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
 function font_awesome_files_included() {
     wp_enqueue_style( 'font-awesome-css', plugins_url( '/css/font-awesome.min.css', __FILE__ ));
 }
